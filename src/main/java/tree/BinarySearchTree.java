a65b8df342a9370aff5798172e2f833d35ec147a
GitAntinus <gantinus@gmail.com>
2019-03-14 14:08:32 +0800

    implemented all exclude retainAll
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -1,13 +1,43 @@
 package tree;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Stack;
 
 public final class BinarySearchTree<E extends Comparable<E>> implements Collection<E> {
-  private Node root = null;
-  private int size = 0;
-  private Class<E> EClass;
+  private Node root;
+  private int size;
+  private boolean last_operation_effected;
+
+  public BinarySearchTree() {
+    root = null;
+    size = 0;
+    last_operation_effected = false;
+  }
+
+  public BinarySearchTree(BinarySearchTree<E> to_copy) {
+    size = to_copy.size;
+    last_operation_effected = to_copy.last_operation_effected;
+    root = deepClone(to_copy.root);
+  }
+
+  public static BinarySearchTree from(BinarySearchTree tree) {
+    return new BinarySearchTree(tree);
+  }
+
+  private Node deepClone(Node to_copy) {
+    if (to_copy == null) return null;
+    Node new_node = new Node(to_copy.data);
+    new_node.leftChild = deepClone(to_copy.leftChild);
+    new_node.rightChild = deepClone(to_copy.rightChild);
+    return new_node;
+  }
+
+  public boolean isLastOperationEffected() {
+    return last_operation_effected;
+  }
 
   public int size() {
     return size;
@@ -17,54 +47,166 @@ public final class BinarySearchTree<E extends Comparable<E>> implements Collecti
     return size == 0;
   }
 
+  public Iterator<E> iterator() {
+    return new LDRIterator(this);
+  }
+
+  @NotNull
+  public Object[] toArray() {
+    Object[] result = new Object[size];
+    Iterator it = new LDRIterator(this);
+    for (int i = 0; i < size; i++) {
+      result[i] = it.next();
+    }
+    return result;
+  }
+
+  @NotNull
+  public <T> T[] toArray(@NotNull T[] a) {
+    Iterator it = new LDRIterator(this);
+    for (int i = 0; i < size; i++) {
+      a[i] = (T) it.next();
+    }
+    return a;
+  }
+
+  public boolean add(E data) {
+    root = insert(root, data);
+    size++;
+    return last_operation_effected;
+  }
+
+  private Node insert(Node root, E data) {
+    if (root == null) return new Node(data);
+    if (root.data.equals(data)) return root;
+    if (root.data.compareTo(data) > 0) root.leftChild = insert(root.leftChild, data);
+    else root.rightChild = insert(root.rightChild, data);
+    return root;
+  }
+
+  public boolean containsAll(@NotNull Collection<?> c) {
+    for (Object o : c) {
+      if (!contains(o)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public boolean contains(Object data) {
     try {
-      return contains(root, EClass.cast(data));
+      return search(root, (E) (data));
     } catch (ClassCastException e) {
       return false;
     }
   }
 
-  private boolean contains(Node root, E value) {
-    if (value.equals(root.data)) return true;
-    if (value.compareTo(root.data) > 0) return contains(root.right, value);
-    return contains(root.left, value);
-  }
+  private boolean search(Node root, E value) {
+    int compare_result = value.compareTo(root.data);
 
-  public Iterator<E> iterator() {
-    return new LDRIterator(this);
-  }
-
-  public Object[] toArray() {
-    return new Object[0];
-  }
-
-  public <T> T[] toArray(T[] a) {
-    return null;
-  }
-
-  public boolean add(E data) {
-    this.root = insert(this.root, data);
-    return true;
-  }
-
-  public boolean remove(Object o) {
-    return false;
-  }
-
-  public boolean containsAll(Collection<?> c) {
+    while (root != null) {
+      if (compare_result == 0) return true;
+      if (compare_result < 0) {
+        root = root.leftChild;
+      } else {
+        root = root.rightChild;
+      }
+    }
     return false;
   }
 
   public boolean addAll(Collection<? extends E> c) {
-    return false;
+    boolean changed = true;
+    for (E e : c) {
+      changed &= add(e);
+    }
+    return changed;
   }
 
   public boolean removeAll(Collection<?> c) {
-    return false;
+    boolean changed = true;
+    for (Object o : c) {
+      changed &= remove(o);
+    }
+    return changed;
   }
 
-  public boolean retainAll(Collection<?> c) {
+  public boolean remove(Object to_remove) {
+    root = delete(root, (E) to_remove);
+    size--;
+    return last_operation_effected;
+  }
+
+  private Node delete(Node root, E e) {
+    Node parent = null;
+    Node to_remove = root;
+    Node after_removed;
+
+    for (
+      int compare_result = e.compareTo(to_remove.data);
+      compare_result != 0;
+      compare_result = e.compareTo(to_remove.data)
+    ) {
+      parent = to_remove;
+      if (compare_result > 0) {
+        to_remove = to_remove.rightChild;
+      } else {
+        to_remove = to_remove.leftChild;
+      }
+
+      if (to_remove == null) {
+        last_operation_effected = false;
+        return root;
+      }
+    }
+
+    if (to_remove.leftChild == null && to_remove.rightChild == null) { // n,n
+      after_removed = null;
+    } else if (to_remove.leftChild != null && to_remove.rightChild == null) { // l,n
+      after_removed = to_remove.leftChild;
+    } else if (to_remove.leftChild == null) { // n,r
+      after_removed = to_remove.rightChild;
+    } else { // l,r
+      after_removed = popMin(to_remove.rightChild);
+      after_removed.leftChild = to_remove.leftChild;
+      after_removed.rightChild = to_remove.rightChild;
+    }
+
+    if (parent == null) {
+      root = after_removed;
+    } else {
+      if (parent.rightChild == to_remove) {
+        parent.rightChild = after_removed;
+      } else {
+        parent.leftChild = after_removed;
+      }
+    }
+    last_operation_effected = true;
+    return root;
+  }
+
+  /**
+   * this method must be called with a root has both leftChild and rightChild
+   * delete the min value Node form the origin tree and return it.
+   *
+   * @param root has both leftChild and rightChild
+   * @return the min value Node
+   */
+  private Node popMin(Node root) {
+    Node parent = root;
+    Node current = root.leftChild;
+
+    while (current.leftChild != null) {
+      parent = current;
+      current = current.leftChild;
+    }
+
+    parent.leftChild = null;
+
+    return current;
+  }
+
+  public boolean retainAll(@NotNull Collection<?> c) {
     return false;
   }
 
@@ -72,18 +214,10 @@ public final class BinarySearchTree<E extends Comparable<E>> implements Collecti
     root = null;
   }
 
-  private Node insert(Node root, E data) {
-    if (root == null) return new Node(data);
-    if (root.data.equals(data)) return root;
-    if (root.data.compareTo(data) > 0) root.left = insert(root.left, data);
-    else root.right = insert(root.right, data);
-    return root;
-  }
-
   protected class Node {
     E data;
-    Node left = null;
-    Node right = null;
+    Node leftChild = null;
+    Node rightChild = null;
 
     Node(E data) {
       this.data = data;
@@ -103,18 +237,18 @@ public final class BinarySearchTree<E extends Comparable<E>> implements Collecti
     }
 
     /*
-    * */
+     * */
     public E next() {
       while (current != null) {
         stack.push(current);
-        current = current.left;
+        current = current.leftChild;
       }
       /*
-      after iteration, L is chosen if current has a left node, and if not, current is chosen
+      after iteration, L is chosen if current has a leftChild node, and if not, current is chosen
       * */
       current = stack.pop();
       Node node = current;
-      current = current.right;
+      current = current.rightChild;
 
       return node.data;
     }
--- src/test/java/BSTTestTest.java
+++ src/test/java/BSTTestTest.java
@@ -5,16 +5,33 @@
 
 import tree.BinarySearchTree;
 
+import java.util.Scanner;
+
 public class BSTTestTest {
   public static void main(String[] args) {
-    BinarySearchTree<Integer> test = new BinarySearchTree<Integer>();
-    test.add(1);
-    test.add(-1);
-    test.add(3);
-    test.add(5);
-    test.add(10);
-    for (Integer integer : test) {
-      System.out.println(integer);
+    BinarySearchTree<Integer> test = new BinarySearchTree<>();
+    Scanner user_input = new Scanner(System.in);
+    while (true) {
+      switch (user_input.next(".").charAt(0)) {
+        case 'a':
+          System.out.println(test.add(user_input.nextInt()));
+          break;
+        case 'r':
+          System.out.println(test.remove(user_input.nextInt()));
+          break;
+        case 'c':
+          System.out.println(test.contains(user_input.nextInt()));
+          break;
+        case 's':
+          for (Integer integer : test) {
+            System.out.print(integer);
+            System.out.print(',');
+          }
+          System.out.println();
+          break;
+        default:
+          System.out.println("operation unknown");
+      }
     }
   }
 }
